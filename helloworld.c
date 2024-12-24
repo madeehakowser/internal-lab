91b075cfdd13404875e6e4cbac4fb4d933e1cb1b
madeehakowser <madiyakowser014@gmail.com>
2024-12-24 14:01:37 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+clrscr();
+printf"enter the value of x:");
+scanf("%d",&a);
+printf("enetr the value of y:");
+scanf("d",&b);
+z=x-y
+printf("d sum of x and y",&z);
+}
+}
